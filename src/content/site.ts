cbfbc120bfb380a7abc6e39dbe7724d52e5cb35d
farahzahirdev <farahzahir.dev@gmail.com>
2026-07-20 16:07:57 +0500

    chore:update map embedded

--- src/content/site.ts
+++ src/content/site.ts
@@ -16,10 +16,9 @@ export const site = {
   website: "https://www.badlandstms.com",
   email: "admin@badlandstms.com",
   emailHref: "mailto:admin@badlandstms.com",
-  mapsUrl:
-    "https://www.google.com/maps/search/?api=1&query=2239+Northeast+Doctors+Drive+Suite+110+Bend+OR+97701",
+  mapsUrl: "https://maps.app.goo.gl/D1uFHTiLguo2WkoH8",
   mapsEmbedUrl:
-    "https://www.google.com/maps?q=2239+Northeast+Doctors+Drive+Suite+110,+Bend,+OR+97701&z=15&output=embed",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2866.629!2d-121.2661238!3d44.0703643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54b8c51c96b8e6f5%3A0xa4f86b1af5edc7b2!2sBadlands%20TMS!5e0!3m2!1sen!2sus!4v1!5m2!1sen!2sus",
   brainswayVideoEmbedUrl: "https://www.youtube.com/embed/LT-CWYi4Spk",
   brainswayVideoTitle: "Badlands TMS Provides BrainsWay Deep TMS Technology",
   social: {
